b824406e076e4c979f195df2acb0d220d704fde6
Vaishak Dinesh <vaishak@cloudflare.com>
2025-10-30 14:46:00 -0700

    fix: add state json to context

--- cmd/tf-migrate/main.go
+++ cmd/tf-migrate/main.go
@@ -288,6 +288,7 @@ func processStateFile(log hclog.Logger, p *pipeline.Pipeline, cfg config) error 
 
 	ctx := &transform.Context{
 		Content:       content,
+		StateJSON:     string(content),
 		Filename:      filepath.Base(cfg.stateFile),
 		Diagnostics:   make(hcl.Diagnostics, 0),
 		Metadata:      make(map[string]interface{}),
