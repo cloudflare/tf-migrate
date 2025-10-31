a95232649d6d0fe1a844de29c860e14773ed5148
cortlyons <cortlyons@cloudflare.com>
2025-10-31 11:52:48 -0600

    feat: v4 to v5 migration for zero trust access service token

--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cloudflare/tf-migrate/internal/resources/account_member"
 	"github.com/cloudflare/tf-migrate/internal/resources/api_token"
 	"github.com/cloudflare/tf-migrate/internal/resources/dns_record"
+	"github.com/cloudflare/tf-migrate/internal/resources/zero_trust_access_service_token"
 	"github.com/cloudflare/tf-migrate/internal/resources/zero_trust_list"
 )
 
@@ -16,4 +17,5 @@ func RegisterAllMigrations() {
 	api_token.NewV4ToV5Migrator()
 	dns_record.NewV4ToV5Migrator()
 	zero_trust_list.NewV4ToV5Migrator()
+	zero_trust_access_service_token.NewV4ToV5Migrator()
 }
