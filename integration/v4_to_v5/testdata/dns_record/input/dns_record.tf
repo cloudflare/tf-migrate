2be746ef7017516e9ab481462dd63a67bc7e6d57
Tamás Józsa <tamas@cloudflare.com>
2025-11-19 14:34:43 +0000

    feat: variables should be part of testdata (#32)

--- integration/v4_to_v5/testdata/dns_record/input/dns_record.tf
+++ integration/v4_to_v5/testdata/dns_record/input/dns_record.tf
@@ -1,3 +1,13 @@
+variable "cloudflare_account_id" {
+  description = "Cloudflare account ID"
+  type        = string
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare zone ID"
+  type        = string
+}
+
 # Standard DNS records
 resource "cloudflare_record" "example_a" {
   zone_id = var.cloudflare_zone_id
