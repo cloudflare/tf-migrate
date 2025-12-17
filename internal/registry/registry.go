dbc10c95a437254016b574de6094a80488ecf765
Vaishak Dinesh <vaishak@cloudflare.com>
2025-12-17 14:09:28 -0800

    feat: migrate regional hostnames (#66)
    * feat(regional_hostname): migration logic
    
    * chore: register  migration
    
    * chore: integration test and test data
    
    * fix: variable names

--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -17,6 +17,7 @@ import (
 	"github.com/cloudflare/tf-migrate/internal/resources/page_rule"
 	"github.com/cloudflare/tf-migrate/internal/resources/pages_project"
 	"github.com/cloudflare/tf-migrate/internal/resources/r2_bucket"
+	"github.com/cloudflare/tf-migrate/internal/resources/regional_hostname"
 	"github.com/cloudflare/tf-migrate/internal/resources/url_normalization_settings"
 	"github.com/cloudflare/tf-migrate/internal/resources/workers_kv"
 	"github.com/cloudflare/tf-migrate/internal/resources/workers_kv_namespace"
@@ -56,6 +57,7 @@ func RegisterAllMigrations() {
 	page_rule.NewV4ToV5Migrator()
 	pages_project.NewV4ToV5Migrator()
 	r2_bucket.NewV4ToV5Migrator()
+	regional_hostname.NewV4ToV5Migrator()
 	url_normalization_settings.NewV4ToV5Migrator()
 	workers_kv.NewV4ToV5Migrator()
 	workers_kv_namespace.NewV4ToV5Migrator()
