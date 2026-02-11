5e3462857e0dceec2afa0eb4659b033d2e13218a
Sarah Sicard <ssicard@cloudflare.com>
2026-02-11 17:45:24 -0600

    add UsesProviderStateUpgrader func

--- internal/resources/origin_ca_certificate/v4_to_v5.go
+++ internal/resources/origin_ca_certificate/v4_to_v5.go
@@ -75,3 +75,8 @@ func (m *V4ToV5Migrator) TransformState(ctx *transform.Context, instance gjson.R
 	// See: cloudflare-terraform-next/internal/services/origin_ca_certificate/migration/v500/
 	return instance.String(), nil
 }
+
+// UsesProviderStateUpgrader indicates that this resource uses provider-based state migration
+func (m *V4ToV5Migrator) UsesProviderStateUpgrader() bool {
+	return true
+}
