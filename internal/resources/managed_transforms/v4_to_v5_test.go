e3391b5735c40695282efa7d0e725fbc83aa3e79
Vaishak Dinesh <vaishak@cloudflare.com>
2026-02-12 15:09:29 -0800

    chore: remove un-needed tests

--- internal/resources/managed_transforms/v4_to_v5_test.go
+++ internal/resources/managed_transforms/v4_to_v5_test.go
@@ -15,10 +15,6 @@ func TestV4ToV5Transformation(t *testing.T) {
 	})
 }
 
-func TestV4ToV5TransformationState_Removed(t *testing.T) {
-	t.Skip("State transformation tests removed - state migration is now handled by provider's StateUpgraders")
-}
-
 func TestUsesProviderStateUpgrader(t *testing.T) {
 	migrator := NewV4ToV5Migrator()
 	if got := migrator.(*V4ToV5Migrator).UsesProviderStateUpgrader(); !got {
@@ -398,4 +394,3 @@ moved {
 
 	testhelpers.RunConfigTransformTests(t, tests, migrator)
 }
-
