91ffc0e675c11d87b57b7743cb5097323b7c90c4
cortlyons <cortlyons@cloudflare.com>
2026-02-20 14:42:34 -0700

    api_shield_operation state upgrader

--- integration/v4_to_v5/testdata/api_shield_operation/expected/api_shield_operation.tf
+++ integration/v4_to_v5/testdata/api_shield_operation/expected/api_shield_operation.tf
@@ -18,7 +18,7 @@ resource "cloudflare_api_shield_operation" "get_users" {
   zone_id  = var.cloudflare_zone_id
   method   = "GET"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users"
+  endpoint = "/cftftest/api/users"
 }
 
 # Test case 2: POST operation with single parameter
@@ -26,7 +26,7 @@ resource "cloudflare_api_shield_operation" "create_user" {
   zone_id  = var.cloudflare_zone_id
   method   = "POST"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users"
+  endpoint = "/cftftest/api/users"
 }
 
 # Test case 3: GET operation with single parameter
@@ -34,7 +34,7 @@ resource "cloudflare_api_shield_operation" "get_user_by_id" {
   zone_id  = var.cloudflare_zone_id
   method   = "GET"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users/{var1}"
+  endpoint = "/cftftest/api/users/{var1}"
 }
 
 # Test case 4: PUT operation with parameter
@@ -42,7 +42,7 @@ resource "cloudflare_api_shield_operation" "update_user" {
   zone_id  = var.cloudflare_zone_id
   method   = "PUT"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users/{var1}"
+  endpoint = "/cftftest/api/users/{var1}"
 }
 
 # Test case 5: DELETE operation with parameter
@@ -50,7 +50,7 @@ resource "cloudflare_api_shield_operation" "delete_user" {
   zone_id  = var.cloudflare_zone_id
   method   = "DELETE"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users/{var1}"
+  endpoint = "/cftftest/api/users/{var1}"
 }
 
 # Test case 6: PATCH operation with multi-parameter endpoint
@@ -58,7 +58,7 @@ resource "cloudflare_api_shield_operation" "update_user_post" {
   zone_id  = var.cloudflare_zone_id
   method   = "PATCH"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users/{var1}/posts/{var2}"
+  endpoint = "/cftftest/api/users/{var1}/posts/{var2}"
 }
 
 # Test case 7: GET operation with multi-parameter endpoint
@@ -66,7 +66,7 @@ resource "cloudflare_api_shield_operation" "get_user_comment" {
   zone_id  = var.cloudflare_zone_id
   method   = "GET"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users/{var1}/posts/{var2}/comments/{var3}"
+  endpoint = "/cftftest/api/users/{var1}/posts/{var2}/comments/{var3}"
 }
 
 # Test case 8: POST operation with subdomain host
@@ -74,7 +74,7 @@ resource "cloudflare_api_shield_operation" "v2_create_resource" {
   zone_id  = var.cloudflare_zone_id
   method   = "POST"
   host     = "v2.api.${var.cloudflare_domain}"
-  endpoint = "/api/resources"
+  endpoint = "/cftftest/api/resources"
 }
 
 # Test case 9: GET operation with health check endpoint
@@ -82,7 +82,7 @@ resource "cloudflare_api_shield_operation" "staging_get_health" {
   zone_id  = var.cloudflare_zone_id
   method   = "GET"
   host     = "api.staging.${var.cloudflare_domain}"
-  endpoint = "/health"
+  endpoint = "/cftftest/health"
 }
 
 # Test case 10: OPTIONS operation (for CORS preflight)
@@ -90,7 +90,7 @@ resource "cloudflare_api_shield_operation" "options_users" {
   zone_id  = var.cloudflare_zone_id
   method   = "OPTIONS"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users"
+  endpoint = "/cftftest/api/users"
 }
 
 # Test case 11: HEAD operation
@@ -98,7 +98,7 @@ resource "cloudflare_api_shield_operation" "head_users" {
   zone_id  = var.cloudflare_zone_id
   method   = "HEAD"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users"
+  endpoint = "/cftftest/api/users"
 }
 
 # Test case 12: POST with versioned API path
@@ -106,7 +106,7 @@ resource "cloudflare_api_shield_operation" "v1_create_order" {
   zone_id  = var.cloudflare_zone_id
   method   = "POST"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/v1/orders"
+  endpoint = "/cftftest/api/v1/orders"
 }
 
 # Test case 13: GET with nested resource path
@@ -114,7 +114,7 @@ resource "cloudflare_api_shield_operation" "get_account_settings" {
   zone_id  = var.cloudflare_zone_id
   method   = "GET"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/v1/accounts/{var1}/settings"
+  endpoint = "/cftftest/api/v1/accounts/{var1}/settings"
 }
 
 # Test case 14: PATCH with query-like endpoint
@@ -122,7 +122,7 @@ resource "cloudflare_api_shield_operation" "search_users" {
   zone_id  = var.cloudflare_zone_id
   method   = "GET"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users/search"
+  endpoint = "/cftftest/api/users/search"
 }
 
 # Test case 15: DELETE with nested resource
@@ -130,5 +130,5 @@ resource "cloudflare_api_shield_operation" "delete_user_session" {
   zone_id  = var.cloudflare_zone_id
   method   = "DELETE"
   host     = "api.${var.cloudflare_domain}"
-  endpoint = "/api/users/{var1}/sessions/{var2}"
+  endpoint = "/cftftest/api/users/{var1}/sessions/{var2}"
 }
