8857fc1a690c599c3a091d49b59a974d146f3c23
Tamas Jozsa <tamas@cloudflare.com>
2026-04-08 18:18:16 +0100

    test(workers_secret): update integration test for new migration behavior

--- integration/v4_to_v5/testdata/workers_secret/expected/workers_secret.tf
+++ integration/v4_to_v5/testdata/workers_secret/expected/workers_secret.tf
@@ -1,46 +1,35 @@
 # Test both v4 resource name variants
 
+# Singular form (deprecated in v4)
+resource "cloudflare_worker_secret" "singular_secret" {
+  name        = "SINGULAR_SECRET"
+  script_name = "my-worker"
+  secret      = "singular-secret-value"
+  # MIGRATION WARNING: cloudflare_workers_secret removed in v5. Migrate this secret to a 'secret_text' binding in cloudflare_workers_script. See: https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/guides/version-5-upgrade#cloudflare_workers_secret
+}
 
 # Plural form (preferred in v4)
 resource "cloudflare_workers_secret" "plural_secret" {
   name        = "PLURAL_SECRET"
   script_name = "my-worker"
-  secret_text = "plural-secret-value"
-  # MIGRATION WARNING: MIGRATION REQUIRED: Add account_id attribute (required in v5)
+  secret      = "plural-secret-value"
+  # MIGRATION WARNING: cloudflare_workers_secret removed in v5. Migrate this secret to a 'secret_text' binding in cloudflare_workers_script. See: https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/guides/version-5-upgrade#cloudflare_workers_secret
 }
 
+# With account_id (should not generate warning)
+resource "cloudflare_worker_secret" "with_account" {
+  account_id  = var.account_id
+  name        = "WITH_ACCOUNT"
+  script_name = "my-worker"
+  secret      = "secret-with-account"
+  # MIGRATION WARNING: cloudflare_workers_secret removed in v5. Migrate this secret to a 'secret_text' binding in cloudflare_workers_script. See: https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/guides/version-5-upgrade#cloudflare_workers_secret
+}
 
 # With dispatch namespace
 resource "cloudflare_workers_secret" "with_namespace" {
   name               = "NAMESPACE_SECRET"
   script_name        = "my-worker"
+  secret             = "namespace-secret-value"
   dispatch_namespace = "my-namespace"
-  secret_text        = "namespace-secret-value"
-  # MIGRATION WARNING: MIGRATION REQUIRED: Add account_id attribute (required in v5)
-}
-
-# Singular form (deprecated in v4)
-resource "cloudflare_workers_secret" "singular_secret" {
-  name        = "SINGULAR_SECRET"
-  script_name = "my-worker"
-  secret_text = "singular-secret-value"
-  # MIGRATION WARNING: MIGRATION REQUIRED: Add account_id attribute (required in v5)
-}
-
-moved {
-  from = cloudflare_worker_secret.singular_secret
-  to   = cloudflare_workers_secret.singular_secret
-}
-
-# With account_id (should not generate warning)
-resource "cloudflare_workers_secret" "with_account" {
-  account_id  = var.account_id
-  name        = "WITH_ACCOUNT"
-  script_name = "my-worker"
-  secret_text = "secret-with-account"
-}
-
-moved {
-  from = cloudflare_worker_secret.with_account
-  to   = cloudflare_workers_secret.with_account
+  # MIGRATION WARNING: cloudflare_workers_secret removed in v5. Migrate this secret to a 'secret_text' binding in cloudflare_workers_script. See: https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/guides/version-5-upgrade#cloudflare_workers_secret
 }
